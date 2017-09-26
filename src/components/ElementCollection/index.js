fc971d378df467853dcd34fb963b3d6c0f28e5ef
lihe3 <lihe3@corp.beisencorp.com>
2017-09-26 17:03:35 +0800

    build:完善pageBuidler接入

--- src/components/ElementCollection/index.js
+++ src/components/ElementCollection/index.js
@@ -1,1 +1,12 @@
-export default []
+/**
+ * 在这个文件中输出所有的组件
+ * **/
+import React from 'react'
+class ComponentHolder extends React.Component{
+  render(){
+    return <div>name</div>
+  }
+}
+export default {
+  ComponentHolder
+}
