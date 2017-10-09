b6a190a0ab595139263819680e8b477641d81c72
lihe3 <lihe3@corp.beisencorp.com>
2017-10-09 18:52:06 +0800

    build:完善打包的配置文件

--- webpack.componet.config.js
+++ webpack.componet.config.js
@@ -1,4 +1,5 @@
+const {projectName} = require('./src/dll-config.js')
 module.exports = require('@beisen/talent-ui-component-webpack-config')({
   entry: './src/index.js',
-  root: require('path').resolve(__dirname,'src/venders/upaas')
+  root: require('path').resolve(__dirname,'src/venders/' + projectName)
 })
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const {dllList} = require('./src/dll-config.js')
+console.log(dllList)
 module.exports = require('@beisen/talent-ui-dll-webpack-config')({
   root: path.resolve(__dirname),
   venders: ['./src/venders/upaas/dist/index.js'],
-  dllList: ['@beisen/talent-ui-dll-foundation']
+  dllList: dllList
 });
