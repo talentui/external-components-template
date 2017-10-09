c988344b9910085be2d22adc95bed9bd9d5c3578
lihe3 <lihe3@corp.beisencorp.com>
2017-10-09 18:49:46 +0800

    build:add dll-config file

---
+++ src/dll-config.js
@@ -0,0 +1,13 @@
+/**
+ * 打包 DLL 的一些配置
+ * 
+ * 1. projectName：项目组件名称，如upaas，在打包以后，会在src/venders/下生成对应的文件夹
+ * 
+ * 2. dllList: 在打包dll的时候，为了防止重复打包，某些外部依赖是不能打包的，例如react等
+ * @beisen/talent-ui-dll-foundation是使用Webpack DllPlugin打包出来的dll 基本上等于talent-libs
+ * 依赖的公共组件是否需要打包为一个dll  ？
+ * **/
+module.exports = {
+  projectName:'upaas',
+  dllList: ['@beisen/talent-ui-dll-foundation']
+}
