3e9d29591aef495064199b19c03a7a9656947950
lihe3 <lihe3@corp.beisencorp.com>
2017-10-09 16:50:36 +0800

    build:add dlllist param

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.exports = require('@beisen/talent-ui-dll-webpack-config')({
   root: path.resolve(__dirname),
-  venders: ['./src/venders/upaas/dist/index.js']
+  venders: ['./src/venders/upaas/dist/index.js'],
+  dllList: ['@beisen/talent-ui-dll-foundation']
 });
