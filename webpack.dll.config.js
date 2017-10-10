f90915ed15ad7446e8bb5345a00d97336cd9b870
lihe3 <lihe3@corp.beisencorp.com>
2017-10-10 15:02:56 +0800

    fix bug : venders path project naem

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const {dllList} = require('./src/dll-config.js')
-console.log(dllList)
+const {dllList, projectName} = require('./src/dll-config.js')
 module.exports = require('@beisen/talent-ui-dll-webpack-config')({
   root: path.resolve(__dirname),
-  venders: ['./src/venders/upaas/dist/index.js'],
+  venders: ['./src/venders/'+projectName+'/dist/index.js'],
   dllList: dllList
 });
