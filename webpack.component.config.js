720ac0c644d7078a96e53dad52b193081c486d89
lihe3 <lihe3@corp.beisencorp.com>
2017-10-30 11:00:08 +0800

    build:remove some webpack config file

--- webpack.component.config.js
+++
@@ -1,8 +0,0 @@
-const {dllList, libraryName} = require('./src/component-config.js')
-module.exports = require('./webpack-config')({
-  entry: './src/index.js',
-  root: require('path').resolve(__dirname),
-  dllList: dllList,
-  libraryName: libraryName,
-  compress:true
-})
