0a0eb2d01451f4568f1832a78b0e86410f09f0a8
lihe3 <lihe3@corp.beisencorp.com>
2017-10-30 10:59:13 +0800

    build:change libraryName ==> library

--- src/component-config.js
+++ src/component-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  libraryName:'UPAAS_PB_COMPONENT',//暴露的全局变量 library
+  library:'UPAAS_PB_COMPONENT',//暴露的全局变量 library
   dllList: ['@talentui/dll-react', '@talentui/dll-ethos']//组件所依赖的dll list
 }
