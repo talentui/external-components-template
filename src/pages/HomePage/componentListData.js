fbb43ae9f69cd30e1c9044097fa92536ba70af32
lihe3 <lihe3@corp.beisencorp.com>
2017-10-30 09:42:25 +0800

    build:delete vender folder

---
+++ src/pages/HomePage/componentListData.js
@@ -0,0 +1,21 @@
+/**
+ * 左侧展现的组件列表
+ * 当拖拽组件到工作区的时候，pageBuilder会根据name渲染ElementCollection中定义的对应组件
+ * 
+ * **/
+import { v1 } from 'uuid';
+import ElementCollection from '../../components/ElementCollection/index'
+let componentsKeys = Object.keys(ElementCollection);
+export default [
+  {
+    name: "组件分组1",
+    id: v1(),
+    components: componentsKeys.map(item =>  {
+      return {
+        name:item,
+        displayTitle:item,
+        id:v1()
+      }
+    })
+  }
+];
---
+++ src/pages/HomePage/page-view.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import GridWorkspace, { TubState } from '@beisen/grid-page-builder';
+// import '@beisen/grid-page-builder/style.css';
+import PageTemplate from '../../components/PageTemplates/index';
+import components from '../../components/ElementCollection';
+import propsComponents from '../../components/PropsCollection';
+import { v1 } from 'uuid';
+import componentListData from './componentListData'
+
+export default class Grid extends Component {
+
+  constructor(props, contents) {
+    super();
+    this.state = { tubState: TubState.create({
+      pageSettings:{
+        title: 'TalentUI-北森',
+        editableData: {},
+        layout: 'grid'
+    }}) };
+    this.availableComponent = this.genAvailableComponent()
+  }
+
+  genAvailableComponent() {
+    return componentListData;
+  }
+
+  handleChange = (tubState) => {
+    this.setState({ tubState })
+  }
+
+  handleSave = (tubState) => {
+    window.localStorage.uibuilder = JSON.stringify(tubState);
+    this.state.tubState.setSavedState();
+    debugger;
+  }
+
+  renderComponent = (Comp, editableData, mergeProps /* */) => {
+    return <div>1111<Comp data={editableData} {...mergeProps} /></div>
+  }
+
+  genId(data) {
+    console.log(data);
+    return v1();
+  }
+
+  createComponent(component, data){
+    return component
+  }
+
+  render() {
+    return (
+      <GridWorkspace
+        tubState={this.state.tubState}
+        template={PageTemplate}
+        components={components}
+        propsComponents={propsComponents}
+        availableComponents={this.availableComponent}
+        onChange={this.handleChange}
+        onSave={this.handleSave}
+        genUID={this.genId}
+        onMessage={this.handleMessage}
+        createComponent = {this.createComponent}
+        previewUrl='#/preview'
+        />
+    );
+  }
+}
