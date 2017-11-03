15fb2cb22ac42b7b80744cbd83b12409d73ce93e
lihe3 <lihe3@corp.beisencorp.com>
2017-11-03 16:34:48 +0800

    chros:

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -1,5 +1,5 @@
 const {dllList, library} = require('./src/component-config.js')
-module.exports = require('./webpack-library-config')({
+module.exports = require('@talentui/webpack-library-config')({
   entry: './src/index.js',
   root: require('path').resolve(__dirname),
   dllList: dllList,
