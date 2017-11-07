cb7e0d038ff9ff94806191c172524e823be84ae3
lihe3 <lihe3@corp.beisencorp.com>
2017-11-07 14:14:08 +0800

    build:调整publicPath

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import eLementCollections from './components/ElementCollection/index.js'
 import propsCollections from './components/PropsCollection/index.js'
 import pageTemplate from './components/PageTemplates/index'
-__webpack_public_path__ = '//xfile5.tita.com/ux/tita-web-v4/release/app/aa';
+__webpack_public_path__ = `"静态资源地址/"${process.env.library}`//全局变量名
+window.pb_umd_components_lists =  window.pb_umd_components_lists || [];
+window.pb_umd_components_lists.push(process.env.library)
 export default {
   eLementCollections:eLementCollections,//所有的组件
   propsCollections:propsCollections,//所有的属性组件
