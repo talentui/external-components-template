b5dca6cff1538ff2df12e8c282e6c65b3a585459
lihe3 <lihe3@corp.beisencorp.com>
2017-11-07 14:16:19 +0800

    build:去掉外部传的library

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -1,11 +1,6 @@
 const {dllList} = require('./src/component-config.js')
-const fs = require('fs');
-const obj = JSON.parse(fs.readFileSync(('./package.json'),'utf8'));
-const rawName = obj.name;
-const library = rawName.replace(/@|\-|\//g,'_');//以package name作为全局的变量名
 module.exports = require('@talentui/webpack-library-config')({
   entry: './src/index.js',
   root: require('path').resolve(__dirname),
-  dllList: dllList,
-  library: library
+  dllList: dllList
 })
