500c912f9026d91e9be505ab7fd676a59bafa3f0
lihe3 <lihe3@corp.beisencorp.com>
2017-11-07 10:09:34 +0800

    build:以package name 作为全局的变量名

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -1,4 +1,8 @@
-const {dllList, library} = require('./src/component-config.js')
+const {dllList} = require('./src/component-config.js')
+const fs = require('fs');
+const obj = JSON.parse(fs.readFileSync(('./package.json'),'utf8'));
+const rawName = obj.name;
+const library = rawName.replace(/@|\-|\//g,'_');//以package name作为全局的变量名
 module.exports = require('@talentui/webpack-library-config')({
   entry: './src/index.js',
   root: require('path').resolve(__dirname),
