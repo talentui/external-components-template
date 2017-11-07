7ad3f6b839c74f77d68355c6e82c8c4ee2caf059
lihe3 <lihe3@corp.beisencorp.com>
2017-11-07 14:16:43 +0800

    build:add comment

--- src/component-config.js
+++ src/component-config.js
@@ -1,3 +1,9 @@
+/**
+ * 目前@talentui提供的打包好的dll有 ：
+ * 1. @talentui/dll-react  https://github.com/talentui/dll-react
+ * 2. @talentui/dll-ethos  https://github.com/talentui/dll-ethos 
+ * **/
+
 module.exports = {
   // library:'UPAAS_PB_COMPONENT',//暴露的全局变量 library 
   dllList: ['@talentui/dll-react', '@talentui/dll-ethos']//组件所依赖的dll list
