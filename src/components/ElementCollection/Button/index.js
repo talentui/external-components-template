d912bc35ad4f9dde2ef42d072bae13fb6c81903b
lihe3 <lihe3@corp.beisencorp.com>
2017-11-20 10:49:11 +0800

    build:remove useless code

--- src/components/ElementCollection/Button/index.js
+++ src/components/ElementCollection/Button/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 // import { SketchPicker } from 'react-color'
 import './index.scss'
-import './a.css'
 export default class Button extends Component {
   static canBroadcast = true;
 
