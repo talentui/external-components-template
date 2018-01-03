d9466e46a3494dd6dbbcd3a3c4d72b0b71ef1e90
lihe3 <lihe3@corp.beisencorp.com>
2018-01-03 11:06:06 +0800

    fix bug : add modulescope

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -2,5 +2,6 @@ const {dllList} = require('./src/component-config.js')
 module.exports = require('@talentui/webpack-library-config')({
   entry: './src/index.js',
   root: require('path').resolve(__dirname),
-  dllList: dllList
+  dllList: dllList,
+  moduleScope:'./src'
 })
