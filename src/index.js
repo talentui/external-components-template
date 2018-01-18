680f0cfc6c7a6b90b959d6ca9bd760084f089e6c
lihe3 <lihe3@corp.beisencorp.com>
2018-01-18 10:58:38 +0800

    build：为组件加前缀

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@
 import eLementCollections from "./components/ElementCollection/index.js";
 import propsCollections from "./components/PropsCollection/index.js";
 import pageTemplate from "./components/PageTemplates/index";
+import prefixAdder from "./utils/prefix";
 __webpack_public_path__ = `//stnew03.beisen.com/ux/upaas/${
     process.env.rawName
 }/release/dist/`; //全局变量名
 window.pb_umd_components_lists = window.pb_umd_components_lists || [];
 window.pb_umd_components_lists.push(process.env.library);
 export default {
-    eLementCollections: eLementCollections, //所有的组件
-    propsCollections: propsCollections, //所有的属性组件
-    pageTemplate: pageTemplate //页面模版组件
+    eLementCollections: prefixAdder(eLementCollections), //所有的组件
+    propsCollections: prefixAdder(propsCollections), //所有的属性组件
+    pageTemplate: prefixAdder(pageTemplate) //页面模版组件
 };
