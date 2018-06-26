a946dafffd38a797c525593e4be8a6edc47610bd
lihe3 <lihe3@corp.beisencorp.com>
2018-06-26 10:03:41 +0800

    build:add examples folder

---
+++ examples/default/index.js
@@ -0,0 +1,7 @@
+import DefaultComponent from '../../src'
+import React, { Component } from 'react'
+export default class Examples extends React.Component{
+    render(){
+        return <DefaultComponent/>
+    }
+}
